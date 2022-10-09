d2b98a067310e5de46d9fa717296682df86ef68d
Elda-Dani <eldadani2001@gmail.com>
2022-10-09 16:49:55 +0530

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const appRoutes:Routes=[
+{
+  path:"",component:HomeComponent
+},
+{
+  path:"about",component:AboutComponent
+},
+{
+  path:"gallery",component:GalleryComponent
+},
+{
+  path:"contact",component:ContactComponent
+}
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +37,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
